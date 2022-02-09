a805578cd9cc136c322a28d018542267f95d05b7
MagdK <kovacsmagda@gmail.com>
2022-02-09 12:49:47 +0100

    JS changes, html changes

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,27 @@
-function clicked(aLink) {
-    // console.log(aLink.target)
-
-    // 1. Remove "active" class
+function loadEvent(){
+        
+    function clicked(aLink) {
+        // console.log(aLink.target)
+    
+        // 1. Remove "active" class
+        // let links = document.querySelectorAll("header a")
+        // for (let link of links) {
+        //     link.classList.remove("active")
+        // }
+        let activeLink = document.querySelector("header a.active")
+        activeLink.classList.remove("active")
+    
+        // 2. Add "active" class to clicked element
+        aLink.target.classList.add("active")
+    }
+    
     let links = document.querySelectorAll("header a")
     for (let link of links) {
-        link.classList.remove("active")
+        link.addEventListener('click', clicked)
     }
-
-    // 2. Add "active" class to clicked element
-    aLink.target.classList.add("active")
-}
-
-let links = document.querySelectorAll("header a")
-for (let link of links) {
-    link.addEventListener('click', clicked)
-}
-
-
-
-
+};
+  
+window.addEventListener('load', loadEvent);
 
 
 
